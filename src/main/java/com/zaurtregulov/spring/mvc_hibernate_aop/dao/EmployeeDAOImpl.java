f297efe71f5119cc81c967ccfd1de4e4417ce039
SolomonVP <Lum7023@yandex.ru>
2022-01-21 19:48:14 +0300

    73_1

--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -17,22 +17,27 @@ public class EmployeeDAOImpl implements EmployeeDAO{
     @Autowired
     private SessionFactory sessionFactory;
 
-
     @Override
     public List<Employee> getAllEmployees() {
-
         Session session = sessionFactory.getCurrentSession();
-
 //        List<Employee> allEmployees = session.createQuery("from Employee", Employee.class).getResultList();
-
         Query<Employee> query = session.createQuery("from Employee", Employee.class);
         List<Employee> allEmployees = query.getResultList();
         return allEmployees;
     }
+
     @Override
     public void saveEmployee(Employee employee) {
         Session session = sessionFactory.getCurrentSession();
-        session.save(employee);
+
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Employee employee = session.get(Employee.class, id);
+        return employee;
     }
 }
 
